7630422525960701ab92e3c795d0f83757c70df2
code-a11ly <gabrielsouza10042002@gmail.com>
2025-09-08 08:08:48 -0300

    feat: made the how section

--- app/src/app/pages/feedback.tsx
+++ app/src/app/pages/feedback.tsx
@@ -3,6 +3,32 @@
 import Image from "next/image";
 import { useEffect } from 'react';
 
+
+type Feed = {
+  name: string;
+  function: string;
+  review: string;
+}
+
+const feedBack: feed[] = [
+  {
+    name: 'Create your Envision account',
+    function: 'Fill your data once and have access aways',
+    review: 'Create Account',
+  },
+  {
+    name: 'Tell us what you are looking for',
+    function: 'We will start looking for your new place right away',
+    review: 'Take a forms',
+  },
+  {
+    name: 'We will get back to you ASAP',
+    function: 'As soon as we find what you want we will let you know',
+    review: 'See new updates',
+  },
+];
+
+
 export default function Header() {
 
   {/*
@@ -16,7 +42,7 @@ export default function Header() {
 
   return (
 
-    <section className="flex flex-col h-screen w-full bg-white justify-center items-center text-center gap-16font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col h-screen w-full bg-lt-gray justify-center items-center text-gray-400 text-center gap-16font-[family-name:var(--font-geist-sans)]">
     {/*<section className="flex flex-col h-screen w-full bg-[url('/assets/building.jpg')] bg-cover bg-center justify-center items-center text-white text-center gap-16font-[family-name:var(--font-geist-sans)]">*/}
 
 
@@ -24,21 +50,22 @@ export default function Header() {
       <div className="absolute inset-0 bg-black bg-opacity-50"></div> */}
       {/* HEADERS */}
 
-      <div className="flex flex-row justify-center items-center text-lt-title-txt">
+      <div className="flex flex-row justify-center items-center">
 
 
         {/* <div className="h-full w-px bg-gray-300" /> Divider */}
 
+        <h1 className="text-4xl md:text-5xl 2xl:text-7xl font-black mb-4 text-md-gray">
+          What do people say <br/>about us?
+        </h1>
+
         <div className="flex flex-col ">
-          <h1 className="text-4xl md:text-5xl 2xl:text-7xl font-black mb-4 text-md-gray">
-            What do people say <br/>about us?
-          </h1>
 
 
         </div>
       </div>
 
-    </section>
+    </div>
 
   );
 }
--- app/src/app/pages/how.tsx
+++ app/src/app/pages/how.tsx
@@ -2,13 +2,44 @@
 'use client'
 import Image from "next/image";
 import { useEffect } from 'react';
+import { ArrowRight } from "lucide-react";
+
+
+type How = {
+  title: string;
+  context: string;
+  catch: string;
+  path: string;
+}
+
+const hows: how[] = [
+  {
+    title: 'Create your Envision account',
+    context: 'Fill your data once and have access aways',
+    catch: 'Create Account',
+    path: '',
+  },
+  {
+    title: 'Tell us what you are looking for',
+    context: 'We will start looking for your new place right away',
+    catch: 'Take a forms',
+    path: '',
+  },
+  {
+    title: 'We will get back to you ASAP',
+    context: 'As soon as we find what you want we will let you know',
+    catch: 'See new updates',
+    path: '',
+  },
+];
+
 
 export default function How() {
 
 
   return (
 
-    <section className="flex flex-col h-screen w-full bg-white justify-center items-center text-center gap-16font-[family-name:var(--font-geist-sans)]">
+    <section className="flex flex-col w-full bg-white justify-center items-center text-center gap-16font-[family-name:var(--font-geist-sans)]">
     {/*<section className="flex flex-col h-screen w-full bg-[url('/assets/building.jpg')] bg-cover bg-center justify-center items-center text-white text-center gap-16font-[family-name:var(--font-geist-sans)]">*/}
 
 
@@ -16,15 +47,42 @@ export default function How() {
       <div className="absolute inset-0 bg-black bg-opacity-50"></div> */}
       {/* HEADERS */}
 
-      <div className="flex flex-row justify-center items-center text-lt-title-txt">
+      <div className="flex flex-col justify-center items-center text-lt-title-txt my-24">
 
 
         {/* <div className="h-full w-px bg-gray-300" /> Divider */}
 
+        <h1 className="text-4xl md:text-5xl 2xl:text-7xl font-black mb-4 text-md-gray">
+          How you can get a new place <br/> with Envision
+        </h1>
+
         <div className="flex flex-col ">
-          <h1 className="text-4xl md:text-5xl 2xl:text-7xl font-black mb-4 text-md-gray">
-            How you can get a new place <br/> with Envision
-          </h1>
+
+          {hows.map((h, key) => (
+            <div key={key} className={((key % 2) != 0)? 'flex my-4' : 'flex flex-row-reverse my-4'}>
+              <div className="bg-lt-gray m-6 rounded-xl p-6 flex flex-col items-start space-y-4 shadow-lg w-64">
+
+              </div>
+              <div className="m-6 flex flex-col items-start space-y-4 w-64">
+                <div className="bg-lt-gray py-3 px-3.5 rounded-xl shadow-md">
+                  <p className="font-black">0{key + 1}</p>
+                </div>
+
+                <h2 className="text-black text-left text-2xl font-bold">{h.title}</h2>
+
+                <p className="text-left text-sm text-gray-500 text-bold leading-relaxed">
+                  {h.context}
+                </p>
+
+                <div className="flex gap-2 text-dk-green text-bold cursor-pointer hover:underline">
+                  {h.catch}
+                  <ArrowRight/>
+                </div>
+              </div>
+            </div>
+          ))}
+          {/* SHIELD */}
+
 
 
         </div>
